433779b1f9e5bfd2eecea3830c9cbc432f51d0c7
Mauro <40062845+mauroficorella@users.noreply.github.com>
2022-10-27 17:44:39 +0200

    fastapi

---
+++ mypics/src/Backend/user_management.py
@@ -0,0 +1,32 @@
+import firebase_admin
+from firebase_admin import db
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+class User(BaseModel):
+    username: str
+    email: str
+    password: str
+
+cred_obj = firebase_admin.credentials.Certificate('./labap-785cc-firebase-adminsdk-gdm5s-736b213798.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+	'databaseURL':'https://labap-785cc-default-rtdb.europe-west1.firebasedatabase.app/'
+	})
+
+ref = db.reference("/")
+
+router = FastAPI()
+    
+@router.post("/users", response_model=User)
+async def create_user(user: User):
+    print(user.username)
+    ref.set(user.json)
+    return {"status":200}
+    
+
+if __name__ == "__main__":
+    uvicorn.run(router, host="127.0.0.1", port=8000)
+#per entrare nella pagina dove provare le post bisogna andare all'indirizzo dove runna il server /docs
+
+
