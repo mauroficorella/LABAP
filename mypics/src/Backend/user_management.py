3699e78297a56e83311e26758b229753448f2051
Mauro <40062845+mauroficorella@users.noreply.github.com>
2022-10-29 12:04:03 +0200

    Update user_management.py

--- mypics/src/Backend/user_management.py
+++ mypics/src/Backend/user_management.py
@@ -9,19 +9,30 @@ class User(BaseModel):
     email: str
     password: str
 
-cred_obj = firebase_admin.credentials.Certificate('./labap-785cc-firebase-adminsdk-gdm5s-736b213798.json')
+cred_obj = firebase_admin.credentials.Certificate('mypics/labap-785cc-firebase-adminsdk-gdm5s-736b213798.json')
 default_app = firebase_admin.initialize_app(cred_obj, {
 	'databaseURL':'https://labap-785cc-default-rtdb.europe-west1.firebasedatabase.app/'
 	})
 
 ref = db.reference("/")
 
+
 router = FastAPI()
-    
-@router.post("/users", response_model=User)
+
+#@router.post("users/{userid}")
+@router.post("/users")
 async def create_user(user: User):
-    print(user.username)
-    ref.set(user.json)
+    username=user.username
+    password=user.password
+    email=user.email
+    #CON ref.update FACCIAMO AGGIORNARE IL DB AGGIUNGENDO SEMPLICEMENTE
+    #CON ref.set INVECE CANCELLA TUTTO E INSERISCE DA ZERO
+    #ref.update({userid: {
+    ref.update({'prova': { #QUA PRATICAMENTE POI QUANDO LO IMPLEMENTIAMO GLI PASSIAMO userid 
+        'username': username,
+        'password': password,
+        'email': email
+        }})
     return {"status":200}
     
 
