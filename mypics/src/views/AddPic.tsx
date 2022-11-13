928abbf942cbd55b53c0ff48af58b891da6838cb
Martina <martina.turbessi95@gmail.com>
2022-11-13 11:07:15 +0100

    User settings front end

--- mypics/src/views/AddPic.tsx
+++ mypics/src/views/AddPic.tsx
@@ -1,28 +1,16 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import { styled, ThemeProvider } from "@mui/material/styles";
-import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import MainAppBar from "./MainAppBar";
 import theme from "./Landing/theme";
 import React from "react";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import red from "@mui/material/colors/red";
-import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
-import CardContent from "@mui/material/CardContent";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import AccountCircle from "@mui/icons-material/AccountCircle";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import ButtonBase from "@mui/material/ButtonBase";
 import Button from "@mui/material/Button";
 //import { DropzoneArea } from "material-ui-dropzone";
 
--- mypics/src/views/UserSettings.tsx
+++ mypics/src/views/UserSettings.tsx
@@ -1,22 +1,28 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { styled, ThemeProvider } from "@mui/material/styles";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import ButtonGroup from "@mui/material/ButtonGroup";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
 import Container from "@mui/material/Container";
-import ImageList from "./ImageList";
 import MainAppBar from "./MainAppBar";
 import theme from "./Landing/theme";
 import React from "react";
-import TextField from "@mui/material/TextField";
-import IconButton from "@mui/material/IconButton";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import BorderColorIcon from "@mui/icons-material/BorderColor";
+import Badge from "@mui/material/Badge";
+import CreateIcon from "@mui/icons-material/Create";
+import Paper from "@mui/material/Paper";
+import AppForm from "./Landing/AppForm";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import Divider from "@mui/material/Divider";
+import SettingsForm from "./SettingsForm";
 
-export default function Homepage() {
+const MyPaper = styled(Paper)({
+  borderRadius: 20,
+  //borderColor: "#000",
+  padding: 20,
+});
+
+export default function UserSettings() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
@@ -24,89 +30,168 @@ export default function Homepage() {
         <MainAppBar />
         <main>
           <Box sx={{ bgcolor: "background.paper", pt: 8, pb: 8 }}></Box>
-          <Box sx={{ flex: 1, display: "flex", justifyContent: "center" }}>
-            <Avatar
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg" //TODO Mettere immagine qui
-              sx={{ width: 128, height: 128 }}
-            />
-          </Box>{" "}
-          <Box sx={{ bgcolor: "background.paper", pt: 5, pb: 2 }}></Box>
-          <Container
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "Space-between",
-              width: 1 / 4,
-            }}
-          >
-            <Typography
-              component="h1"
-              align="left"
-              color="text.primary"
-              fontSize="15pt"
+          <SettingsForm>
+            <Box
+              sx={{
+                flex: 1,
+                display: "flex",
+                justifyContent: "center",
+                mb: 10,
+              }}
             >
-              Change profile image
-            </Typography>
-            <IconButton aria-label="delete">
-              <CloudUploadIcon />
-            </IconButton>
-          </Container>
-          <Container
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "Space-between",
-              width: 1 / 4,
-            }}
-          >
-            <Typography
-              component="h1"
-              align="left"
-              color="text.primary"
-              fontSize="15pt"
+              <Badge
+                badgeContent={<CreateIcon fontSize="small" />}
+                color="secondary"
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                overlap="circular"
+                sx={{
+                  "& .MuiBadge-badge": {
+                    fontSize: 8,
+                    height: 30,
+                    minWidth: 30,
+                  },
+                }}
+              >
+                <Avatar
+                  alt="Remy Sharp"
+                  src="/static/images/avatar/1.jpg" //TODO Mettere immagine qui
+                  sx={{ width: 128, height: 128 }}
+                />
+              </Badge>
+            </Box>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={9}>
+                <Container
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    fontSize="10pt"
+                    sx={{ mb: 0.5 }}
+                    color="#9e9e9e"
+                  >
+                    Name
+                  </Typography>
+                  <Typography variant="h5" fontSize="15pt" sx={{ mb: 0.5 }}>
+                    Username
+                  </Typography>
+                  <Divider light sx={{ mb: 1.5 }} />
+                </Container>
+              </Grid>
+              <Grid item xs={2}>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  component="a"
+                  //href="//"
+                  sx={{ minWidth: 70, ml: 1 }}
+                >
+                  Edit
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={9}>
+                <Container
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    fontSize="10pt"
+                    sx={{ mb: 0.5, mt: 1 }}
+                    color="#9e9e9e"
+                  >
+                    Email
+                  </Typography>
+                  <Typography variant="h5" fontSize="15pt" sx={{ mb: 0.5 }}>
+                    email@prova.it
+                  </Typography>
+                  <Divider light sx={{ mb: 1.5 }} />
+                </Container>
+              </Grid>
+              <Grid item xs={2}>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  component="a"
+                  //href="//"
+                  sx={{ minWidth: 70, ml: 1 }}
+                >
+                  Edit
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item xs={9}>
+                <Container
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <Typography
+                    variant="h5"
+                    fontSize="10pt"
+                    sx={{ mb: 0.5, mt: 1 }}
+                    color="#9e9e9e"
+                  >
+                    Password
+                  </Typography>
+                  <Typography variant="h5" fontSize="15pt" sx={{ mb: 0.5 }}>
+                    **********
+                  </Typography>
+                  <Divider light />
+                </Container>
+              </Grid>
+              <Grid item xs={2}>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  component="a"
+                  //href="//"
+                  sx={{ minWidth: 70, ml: 1 }}
+                >
+                  Edit
+                </Button>
+              </Grid>
+            </Grid>
+            <Box sx={{ pt: 4, pb: 4 }}></Box>
+            <Container
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
             >
-              Change username
-            </Typography>
-            <IconButton aria-label="delete">
-              <BorderColorIcon />
-            </IconButton>
-          </Container>
-          <Container
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "Space-between",
-              width: 1 / 4,
-            }}
-          >
-            <Typography
-              component="h1"
-              align="left"
-              color="text.primary"
-              fontSize="15pt"
-            >
-              Change email
-            </Typography>
-            <IconButton aria-label="delete">
-              <BorderColorIcon />
-            </IconButton>
-          </Container>
-          <Container
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "Space-between",
-              width: 1 / 4,
-            }}
-          >
-            <Typography component="h1" align="left" color="text.primary" fontSize = "15pt">
-              Change password
-            </Typography>
-            <IconButton aria-label="delete">
-              <BorderColorIcon />
-            </IconButton>
-          </Container>
+              <Button
+                color="secondary"
+                variant="contained"
+                component="a"
+                //href="//"
+                sx={{ minWidth: 100 }}
+              >
+                Logout
+              </Button>
+              <Button
+                color="secondary"
+                variant="contained"
+                component="a"
+                //href="//"
+                sx={{ minWidth: 180 }}
+              >
+                Delete account
+              </Button>
+            </Container>
+          </SettingsForm>
         </main>
       </React.Fragment>
     </ThemeProvider>
