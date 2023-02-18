a9019c1d3bd8e91ea3e456cef08afb15d3d14ff3
Mauro <40062845+mauroficorella@users.noreply.github.com>
2023-02-18 09:48:57 +0100

    Update add_indices_script.py

--- mypics/src/Backend/Elasticsearch/add_indices_script.py
+++ mypics/src/Backend/Elasticsearch/add_indices_script.py
@@ -4,20 +4,22 @@ import requests
 
 def run_script():
 
-    requests.get("http://host.docker.internal:82/addservice")
+    requests.get("http://localhost:82/addservice")
 
     print("ciao")
-    r1 = requests.get(url = "http://host.docker.internal:9200/images/_search")
+    r1 = requests.get(url = "http://localhost:9200/images/_search")
     data1 = r1.json()
     
     arr = []
-    for elem in data1["hits"]["hits"]:
-        arr.append(elem["_source"]["uri"])
+    print(data1)
+    if not(data1["error"]):
+        for elem in data1["hits"]["hits"]:
+            arr.append(elem["_source"]["uri"])
     
     print("ARRAY---------")
     print(arr)
     
-    r2 = requests.get(url = "http://host.docker.internal:81/get_all_posts_urls")
+    r2 = requests.get(url = "http://localhost:81/get_all_posts_urls")
     data2 = r2.json()
     print("Num posts: "+ str(len(data2)))
 
@@ -35,7 +37,7 @@ def run_script():
                         "best": 3,
                         "template": "{ {{#body}}{{#predictions}} \"uri\":\"{{uri}}\",\"categories\": [ {{#classes}} { \"category\":\"{{cat}}\",\"score\":{{prob}} } {{^last}},{{/last}}{{/classes}} ] {{/predictions}}{{/body}} }",
                         "network": {
-                            "url": "http://host.docker.internal:9200/images/img",
+                            "url": "http://localhost:9200/images/img",
                             "http_method": "POST"
                         }
                     }
@@ -46,7 +48,7 @@ def run_script():
             }
             
             jsondata = json.dumps(myobj)
-            r = requests.post("http://host.docker.internal:8080/predict", jsondata)
+            r = requests.post("http://localhost:8080/predict", jsondata)
 
 
 
