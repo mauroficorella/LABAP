f5ee71ba55200f3e48de8605abad50f4b07bc765
Martina <martina.turbessi95@gmail.com>
2023-02-18 10:16:02 +0100

    Update add_indices_script.py

--- mypics/src/Backend/Elasticsearch/add_indices_script.py
+++ mypics/src/Backend/Elasticsearch/add_indices_script.py
@@ -4,10 +4,12 @@ import requests
 
 def run_script():
 
-    requests.get("http://localhost:82/addservice")
+    #requests.get("http://localhost:82/addservice")
+    requests.get("http://host.docker.internal:82/addservice")
 
     print("ciao")
-    r1 = requests.get(url = "http://localhost:9200/images/_search")
+    #r1 = requests.get(url = "http://localhost:9200/images/_search")
+    r1 = requests.get(url = "http://host.docker.internal:9200/images/_search")
     data1 = r1.json()
     
     arr = []
@@ -19,7 +21,8 @@ def run_script():
     print("ARRAY---------")
     print(arr)
     
-    r2 = requests.get(url = "http://localhost:81/get_all_posts_urls")
+    #r2 = requests.get(url = "http://localhost:81/get_all_posts_urls")
+    r2 = requests.get(url = "http://host.docker.internal:81/get_all_posts_urls")
     data2 = r2.json()
     print("Num posts: "+ str(len(data2)))
 
@@ -37,7 +40,8 @@ def run_script():
                         "best": 3,
                         "template": "{ {{#body}}{{#predictions}} \"uri\":\"{{uri}}\",\"categories\": [ {{#classes}} { \"category\":\"{{cat}}\",\"score\":{{prob}} } {{^last}},{{/last}}{{/classes}} ] {{/predictions}}{{/body}} }",
                         "network": {
-                            "url": "http://localhost:9200/images/img",
+                            #"url": "http://localhost:9200/images/img",
+                            "url": "http://host.docker.internal:9200/images/img",
                             "http_method": "POST"
                         }
                     }
@@ -48,9 +52,8 @@ def run_script():
             }
             
             jsondata = json.dumps(myobj)
-            r = requests.post("http://localhost:8080/predict", jsondata)
-
-
+            #r = requests.post("http://localhost:8080/predict", jsondata)
+            r = requests.post("http://host.docker.internal:8080/predict", jsondata)
 
 
 
